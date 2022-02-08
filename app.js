e8746dfca4316e1fd4aa5b07a02c29c83347d493
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-08 19:54:59 +0100

    2

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-let i;
+let dentroHtml = document.getElementById('container');
 
-for (let i = 1; i < 101 ; i++){
+
+for (let i = 1; i <= 100 ; i++){
+     
+     if (i % 3 === 0 && i % 5 === 0  ){
+        console.log('fizzbuzz')
+    }
+    else if(i % 3 === 0){
+         console.log(   'fizz') 
+    }
+    else if (i % 5 === 0){
+        console.log(   'buzz')
+    }
+    else{
+        console.log(i)
+    }
+    
+    
+    
    
 }
 
-let documento = document.getElementById('container');
 
-documento.innerHTML = 
+
+
+
 
