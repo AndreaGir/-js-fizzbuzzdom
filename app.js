84bfa687395fa4748fcc4221ed22ae65b799e904
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-08 20:06:06 +0100

    3

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ let dentroHtml = document.getElementById('container');
 
 
 for (let i = 1; i <= 100 ; i++){
+
+    const div = document.createElement('div')
+    div.classList.add('divDimensioni')
      
      if (i % 3 === 0 && i % 5 === 0  ){
         console.log('fizzbuzz')
+        div.append('fizzbuzz')
+        div.classList.add('bc-rd')
     }
     else if(i % 3 === 0){
          console.log(   'fizz') 
+         div.append('fizz')
+         div.classList.add('bc-yl')
     }
     else if (i % 5 === 0){
         console.log(   'buzz')
+        div.append('buzz')
+        div.classList.add('bc-gr')
     }
     else{
         console.log(i)
+        div.append(i)
     }
     
     
-    
+    dentroHtml.append(div)
    
 }
 
